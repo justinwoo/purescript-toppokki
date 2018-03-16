b4aed8a87db0befd2596542c61405df87b8f5c1d
justinwoo <moomoowoo@gmail.com>
2018-03-16 11:34:49 +0200

    add on page error and load

--- test/Main.purs
+++ test/Main.purs
@@ -2,13 +2,26 @@ module Test.Main where
 
 import Prelude
 
-import Control.Monad.Eff.Console (log)
+import Control.Monad.Aff (launchAff_, makeAff)
+import Control.Monad.Eff.Class (liftEff)
+import Control.Monad.Eff.Ref (newRef, readRef, writeRef)
 import Control.Monad.Eff.Uncurried as EU
+import Data.Maybe (Maybe(..), isJust)
+import Data.Monoid (mempty)
 import Data.String as String
+import Node.Process (cwd)
 import Test.Unit (suite, test)
 import Test.Unit.Assert as Assert
 import Test.Unit.Main (runTest)
 import Toppokki as T
+
+gotoAndLoad' url page aff = makeAff \cb -> do
+  T.onLoad (EU.mkEffFn1 \_ -> launchAff_ do
+    aff
+    liftEff $ cb $ pure unit
+    ) page
+  launchAff_ $ T.goto url page
+  pure mempty
 
 main :: _
 main = runTest do
@@ -21,5 +34,18 @@ main = runTest do
       Assert.assert "content is non-empty string" (String.length content > 0)
       _ <- T.screenshot {path: "./test/test.png"} page
       _ <- T.pdf {path: "./test/test.pdf"} page
-      T.onLoad (EU.mkEffFn1 $ \_ -> log "sdfdsf") page
       T.close browser
+    test "can listen for errors and page load" do
+      browser <- T.launch
+      page <- T.newPage browser
+      dir <- liftEff cwd
+      let url = T.URL
+              $ "file://"
+             <> dir
+             <> "/test/crash.html"
+      ref <- liftEff $ newRef Nothing
+      liftEff $ T.onPageError (EU.mkEffFn1 $ writeRef ref <<< Just) page
+      gotoAndLoad' url page do
+        value <- liftEff $ readRef ref
+        Assert.assert "error occurs from crash.html" $ isJust value
+        T.close browser
