465f0477837b5eed18a4f2a96ab0bacde9049782
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2019-08-11 13:35:46 +0200

    Make whitespace more consistent

--- src/Toppoki.purs
+++ src/Toppoki.purs
@@ -181,10 +181,10 @@ runPromiseAffE2 :: forall a b o. FU.Fn2 a b (Effect (Promise o)) -> a -> b -> Af
 runPromiseAffE2 f a b = Promise.toAffE $ FU.runFn2 f a b
 
 runPromiseAffE3 :: forall a b c o. FU.Fn3 a b c (Effect (Promise o)) -> a -> b -> c -> Aff o
-runPromiseAffE3 f a b c =  Promise.toAffE $ FU.runFn3 f a b c
+runPromiseAffE3 f a b c = Promise.toAffE $ FU.runFn3 f a b c
 
 runPromiseAffE4 :: forall a b c d o. FU.Fn4 a b c d (Effect (Promise o)) -> a -> b -> c -> d -> Aff o
-runPromiseAffE4 f a b c d =  Promise.toAffE $ FU.runFn4 f a b c d
+runPromiseAffE4 f a b c d = Promise.toAffE $ FU.runFn4 f a b c d
 
 foreign import puppeteer :: Puppeteer
 foreign import _launch :: forall options. FU.Fn1 options (Effect (Promise Browser))
